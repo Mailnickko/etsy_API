773b91e503b87e677e5143f9520f11d6bc1f2721
Nicholas Ko <mailnickko@gmail.com>
2016-10-14 23:47:36 -0700

    (feat) Add basic structure for client

---
+++ client/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Goodbye World</h1>
+    );
+  }
+}
+
+export default App;
